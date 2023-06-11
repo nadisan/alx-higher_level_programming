913953728b7dc554da14ffb3c77b072b2d27497b
Nadia <nadiawellela@gmail.com>
2023-06-10 21:12:09 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,7 +10,7 @@ if len(sys.argv) >= 2:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments:".format(len(sys.argv) -1))
+        print("{} arguments:".format(len(sys.argv) - 1))
         i = 1
         while (i < len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
