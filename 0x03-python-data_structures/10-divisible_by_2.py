590c7c65492213a21de039ceec54fee025057a1e
Nadia <nadiawellela@gmail.com>
2023-06-11 20:29:46 -0700

    divisible_by_2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,12 +2,7 @@
 
 def divisible_by_2(my_list=[]):
     i = 0
-    if (len(my_list) > 0):
-        if ((my_list[0] % 2) == 0):
-            div2 = [True]
-        else:
-            div2 = [False]
-    i += 1
+    div2 = []
     while (i < len(my_list)):
         if ((my_list[i] % 2) == 0):
             div2 += [True]
