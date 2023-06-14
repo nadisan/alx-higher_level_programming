4e57d1a12baf9d57f0d6596e8e0fa5fd58eb72ce
Nadia <nadiawellela@gmail.com>
2023-06-13 19:56:10 -0700

    plindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - adds a new node at the beginning of a list_t
+ * @head: list_t
+ *
+ * Return: int
+ */
+
+
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *fw = *head;
+
+	if (fw == NULL || fw->next == NULL)
+	{
+		 return (1);
+	}
+
+	return (1);
+
+}
