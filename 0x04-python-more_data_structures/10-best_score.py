fd677fabb68245ec73572263ebf2dd09b7529924
Nadia <nadiawellela@gmail.com>
2023-06-18 03:32:31 -0700

    best_score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+def best_score(a_dictionary):
+    best ="None"
+    H = 0
+    if (a_dictionary):
+        for k in a_dictionary:
+            if a_dictionary[k] >= H:
+                H = a_dictionary[k]
+                best = k
+    return best
