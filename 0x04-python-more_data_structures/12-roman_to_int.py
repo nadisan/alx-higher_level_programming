d91381489f786bd537f325b9e22d55d53017972f
Nadia <nadiawellela@gmail.com>
2023-06-18 05:31:48 -0700

    roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,18 +5,18 @@ def roman_to_int(roman_string):
     for k in roman_string:
         if k == "I":
             num += (1)
-        if (k == "V" and (num == 0 or num >= 5)):
+        elif (k == "V" and (num == 0 or num >= 5)):
             num += 5
-        if (k == "V" and num < 5):
+        elif (k == "V" and num < 5):
             num = 5 - num
-        if (k == "X" and (num > 0 and num < 10)):
+        elif (k == "X" and (num > 0 and num < 10)):
             num = 10 - num
-        if (k == "X" and (num == 0 or num >= 10)):
+        elif (k == "X" and (num == 0 or num >= 10)):
             num += (10)
-        if k == "L":
+        elif k == "L":
             num += (50)
-        if k == "C":
+        elif k == "C":
             num += (100)
-        if k == "D":
+        elif k == "D":
             num += (500)
     return num
