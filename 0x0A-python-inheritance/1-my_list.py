7df4ebe490a6b25f2527434173c43d7d8e79d660
Nadia <nadiawellela@gmail.com>
2023-07-10 09:46:56 -0700

    updates

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class MyList that inherits from list"""
+
+
+class MyList(list):
+    """class with method print_sorted list"""
+    pass
+
+    def print_sorted(self):
+        
+        """Defines a class MyList that inherits from list
+        
+        args:
+            self: list that is inherited
+        prints: sorted list"""
+        
+        print(sorted(list(self)))
