761d550241c5ae89710be70dfc356c618d8aef7d
Nadia <nadiawellela@gmail.com>
2023-07-10 09:04:16 -0700

    updates

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines lookup function"""
+
+
+def lookup(obj):
+    """
+    Defines a funuction that
+    returns the list of available attributes and methods of an object:
+    arg:
+        obj: object to look up
+    Return: a list object.
+    """
+    pass
