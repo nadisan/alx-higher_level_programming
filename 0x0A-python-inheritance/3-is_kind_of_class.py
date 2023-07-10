c6960c77c8d49d6bac292b7c2e32cec4ffd4120e
Nadia <nadiawellela@gmail.com>
2023-07-10 10:38:45 -0700

    updates

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines function that checks an instance of the specified class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Defines checks if instance is specified class
+    """
+    return (isinstance(obj, a_class))
