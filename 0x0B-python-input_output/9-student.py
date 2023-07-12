bb31134c60713794c6b44183b7e0e79b4d888a4b
Nadia <nadiawellela@gmail.com>
2023-07-11 19:45:37 -0700

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,11 +4,12 @@
 
 class Student:
     """class with  student object"""
+
     def _init_(self, first_name, last_name, age)
-    """initiate student object"""
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        """initiate student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """retrieves a dictionary representation of a Student"""
