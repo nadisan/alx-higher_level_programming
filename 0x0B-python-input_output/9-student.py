726aecf65ff11958d526276cbe3b9ff312be89bd
Nadia <nadiawellela@gmail.com>
2023-07-11 19:48:35 -0700

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,9 +4,8 @@
 
 class Student:
     """class with  student object"""
-
-    def _init_(self, first_name, last_name, age)
-        """initiate student object"""
+    def _init_(self, first_name, last_name, age):
+        """initiate  student object"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
