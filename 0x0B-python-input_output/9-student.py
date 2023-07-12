249db4977a24009cc63b0a482fab80607e834968
Nadia <nadiawellela@gmail.com>
2023-07-11 19:44:07 -0700

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines class for student creation"""
+
+
+class Student:
+    """class with  student object"""
+    def _init_(self, first_name, last_name, age)
+    """initiate student object"""
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student"""
+        return self._dict_
