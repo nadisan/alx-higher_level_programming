bef0b2d0a7628c2aa56b361f1c3f87f8c3347829
Nadia <nadiawellela@gmail.com>
2023-07-16 06:28:49 -0700

    testbase

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,7 @@
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 from io import StringIO
 import sys
 import json
@@ -57,14 +58,38 @@ class TestBase(unittest.TestCase):
         list_output = Rectangle.from_json_string(json_list_input)
         self.assertTrue(list_input == list_output)
 
-    def test_empty(self):
-        """ check empty argument"""
-        Base._Base_nb_objects = 0
-        r1 = Rectangle(1, 1, 1, 1)
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            r1.width = None
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            r1.width = ""
+    def test_create(self):
+        """check load from file"""
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(3, 5, 1)
+        r1_dic = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dic)
+        self.assertFalse(r1 is r2)
+        self.assertFalse(r1 == r2)
+
+    def test_loadFromFile(self):
+        """check save_to_file"""
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(3, 2)
+        list_rect_input = [r1, r2]
+        Rectangle.save_to_file(list_rect_input)
+        list_rect_output = Rectangle.load_from_file()
+        self.assertTrue(type(list_rect_output) == list)
+        for rec in list_rect_input:
+            self.assertTrue(isinstance(rec, Rectangle))
+        for rec in list_rect_output:
+            self.assertTrue(isinstance(rec, Rectangle))
+        s1 = Square(10)
+        s2 = Square(3, 2, 10)
+        list_sq_input = [s1, s2]
+        Square.save_to_file(list_sq_input)
+        list_sq_output = Square.load_from_file()
+        self.assertTrue(type(list_sq_output) == list)
+        for sq in list_sq_input:
+            self.assertTrue(isinstance(sq, Square))
+        for rec in list_rect_output:
+            self.assertTrue(isinstance(sq, Square))
 
     def test_pep8_model(self):
         p8 = pep8.StyleGuide(quiet=True)
