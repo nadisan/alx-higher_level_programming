41173dcae73499744670646a46cddc4564d0f2ce
Nadia <nadiawellela@gmail.com>
2023-08-15 09:28:07 -0700

    task11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,3 @@
 -- a script that lists all records of the table second_table of the database hbtn_0c_0
-SELECT  score, name FROM second_table HAVING Score >= 10
+SELECT score, name FROM second_table
+WHERE score >= 10 ORDER BY score DESC;
