79087db8d3454de45f64c63c46977ceb56e45fee
Nadia <nadiawellela@gmail.com>
2023-08-15 11:06:14 -0700

    15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
 -- script that lists the number of records with the same score in the table 
-SELECT score, COUNT(*) As 'number' FROM second_table GROUP BY score;
+SELECT score, COUNT(*) As 'number' FROM second_table GROUP BY score
+ORDER BY number DESC;
