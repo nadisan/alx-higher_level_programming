b4618e0e0cb5cc580ede5611dab998baab777c0d
Nadia <nadiawellela@gmail.com>
2023-09-11 02:51:25 -0700

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 
-let n = parseInt(process.argv[2]);
-const m = n;
-while (n > 0) {
-  console.log('X'.repeat(m));
-  n--;
+if (parseInt(process.argv[2])){
+ let n = 0;
+ while (n < parseInt(process.argv[2])) {
+  console.log('X'.repeat(parseInt(process.argv[2])));
+  n++;
+ }
+}else {
+ console.log("Missing size");
 }
