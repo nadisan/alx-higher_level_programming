ce986731cdc9fb107e977e3da2144ed0116ee552
Nadia <nadiawellela@gmail.com>
2023-09-11 02:23:24 -0700

    task6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 let n = 0;
-let myVar = ['C is fun','Python is cool','JavaScript is amazing'];
-while (n < 3){
-	console.log(myVar[n]);
-	n++;
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+while (n < 3) {
+  console.log(myVar[n]);
+  n++;
 }
