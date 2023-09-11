1c140987b03940d6527b5d6aad598a50cf4b9098
Nadia <nadiawellela@gmail.com>
2023-09-11 08:03:51 -0700

    101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // prints a message depending of the number of arguments passed
 
-exports.callMeMoby = function (x, theFunction){
- while (x > 0){
-  x--;
-  theFunction();
- }
-}
+exports.callMeMoby = function (x, theFunction) {
+  while (x > 0) {
+    x--;
+    theFunction();
+  }
+};
