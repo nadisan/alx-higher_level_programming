660f47d269970d1acd620aade91fc9c1fbd5c23f
Nadia <nadiawellela@gmail.com>
2023-09-11 03:58:55 -0700

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// prints a message depending of the number of arguments passed
+
+function fact(a) {
+ if (a === 1 || isNaN(a)){
+	 return (1);
+ }else if (a > 1){
+  return (a * fact(a - 1));
+ }
+}
+
+console.log(fact(parseInt(process.argv[2])));
