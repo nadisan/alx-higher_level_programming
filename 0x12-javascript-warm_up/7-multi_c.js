f4c2632e27cba25acb0b181b7e3b6b71b3f5791d
Nadia <nadiawellela@gmail.com>
2023-09-11 02:25:11 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 let n = parseInt(process.argv[2]);
-let myVar = 'C is fun';
-while (n){
-	console.log(myVar);
-	n--;
+const myVar = 'C is fun';
+while (n) {
+  console.log(myVar);
+  n--;
 }
