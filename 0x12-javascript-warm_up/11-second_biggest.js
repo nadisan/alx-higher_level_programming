4c274674851c81d0be4e6f5eb6671c188d576b1f
Nadia <nadiawellela@gmail.com>
2023-09-11 04:31:10 -0700

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// prints a message depending of the number of arguments passed
+
+let arr = [];
+
+if (process.argv.length <= 3){
+	console.log(0)
+}else {
+ const arr = process.argv.sort();
+ console.log(arr.reverse()[1]);
+}
+
