0066d132a74d0c75cb0e3673c7cc133d7db92f37
Nadia <nadiawellela@gmail.com>
2023-09-12 06:20:21 -0700

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }}	
+     print(){
+      while (this.height){
+	console.log("X".repeat(this.width));
+	this.height--;
+     }
+    }
+  }
+
+module.exports = Rectangle;
