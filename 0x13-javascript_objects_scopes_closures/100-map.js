dfe65c72842cf177f627aed3a5fbf038ff29c0dc
Nadia <nadiawellela@gmail.com>
2023-09-12 18:17:21 -0700

    100-map.js

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const list = require('./100-data').list;
+ console.log(list);
+ const mappedlist = list.map(function (e, index){
+ return (e * index)
+ });
+ console.log(mappedlist);
