95fbffe78a828f864d74d667a891f9a7ea67186d
Nadia <nadiawellela@gmail.com>
2023-09-15 01:06:04 -0700

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# lists all states from the database hbtn_0e_0_usa:
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities`  ORDER BY `cities`.`id`")
+    [print(city) for city in c.fetchall()]
