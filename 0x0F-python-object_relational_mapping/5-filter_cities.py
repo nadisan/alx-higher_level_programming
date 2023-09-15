d84be156b3c6c01365af7a38e72d6883e3528e97
Nadia <nadiawellela@gmail.com>
2023-09-15 02:15:31 -0700

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     c = db.cursor()
     c.execute("SELECT cities.id, cities.name, states.name FROM cities \
             JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
-    print(", ".join(city[1] for city in c.fetchall() if city[2] == sys.argv[4]))
+    print(", ".join(ct[1] for ct in c.fetchall() if ct[2] == sys.argv[4]))
