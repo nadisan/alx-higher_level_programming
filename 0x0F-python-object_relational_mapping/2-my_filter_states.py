3473605abd7ffa94471edd433b0c969ae86c2eca
Nadia <nadiawellela@gmail.com>
2023-09-15 00:12:17 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     """ Module matches argument withd db content"""
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1] == {}.format(sys.argv[4])]
+    c.execute("SELECT * FROM `states` WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
