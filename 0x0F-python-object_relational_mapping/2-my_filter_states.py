926851d3b4fc6ff1609ccbd9a4df34eee3bd93ac
Nadia <nadiawellela@gmail.com>
2023-09-14 23:42:42 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" takes in an argument and displays all values in the states table of
-hbtn_0e_0_usa where name matches the argument"""
+"""displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
+    """ Module matches argument withd db content"""
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states` ORDER BY `id`")
