36f14fb0f50b17602de6f7620cebaa468ad4ece8
Nadia <nadiawellela@gmail.com>
2023-09-16 09:21:47 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" lists all State objects from the database hbtn_0e_6_usa"""
+""" lists all State objects from the database hbtn_0e_6_usa
+Usage: ./7-model_state_fetch_all.py <mysql username> /
+                                     <mysql password> /
+                                     <database name>"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
