0c692b0876ea0ebb1c009aeb75d38c045b218057
Nadia <nadiawellela@gmail.com>
2023-09-17 01:33:17 -0700

    task13

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,5 +12,5 @@ Base = declarative_base()
 class State(Base):
     """links to the MySQL table states"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128))
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
