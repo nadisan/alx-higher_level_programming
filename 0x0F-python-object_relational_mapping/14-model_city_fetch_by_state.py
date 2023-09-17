9b459d11e439b135f3d77756ad2f69f496e4bb5f
Nadia <nadiawellela@gmail.com>
2023-09-17 00:59:30 -0700

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     session = Session()
 
     for city, state in session.query(City, State)\
-                               .filter(City.state_id == State.id)\
-                               .order_by(City.id):
+            .filter(City.state_id == State.id)\
+            .order_by(City.id):
         print("{}: {} {}".format(state.name, city.id, city.name))
