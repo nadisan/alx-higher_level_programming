4dac52f852e600d740a86e8ae6431d13d4bc357e
Nadia <nadiawellela@gmail.com>
2023-09-17 01:09:43 -0700

    model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,7 +10,13 @@ Base = declarative_base()
 
 
 class City(Base):
-    """links to the MySQL table states"""
+    """links to the MySQL table cities
+
+    Attributes:
+        id(str): The city's id.
+        name (sqlalchemy.Integer):The city's name.
+        state_id (sqlalchemy.String): The city's state id.
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
