118c89922380d6cfdbbae9cd0c9ed2db0e1037f3
Nadia <nadiawellela@gmail.com>
2023-09-30 02:55:24 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 """Fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-with urllib.request.urlopen('http://python.org/') as response:
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body  response:")
     print("\t- type:{}".format(type(html)))
