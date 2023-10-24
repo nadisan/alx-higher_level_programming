b387bef05d02e2a041fbeb268b81938d48658450
nadisan <nadiawellela@gmail.com>
2023-10-24 05:01:34 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,8 +2,8 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (error, data) {
- if (error){
-  console.log(error);
- }
- })
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (error) {
+  if (error) {
+    console.log(error);
+   }
+  });
