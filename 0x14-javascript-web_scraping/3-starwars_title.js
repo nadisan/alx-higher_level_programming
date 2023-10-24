3a6e0ca80b1cd8dad5514d346b4e0a523f86ae49
nadisan <nadiawellela@gmail.com>
2023-10-24 05:19:25 -0700

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-let starwars = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+const starwars = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
 request(starwars, function (_error, _response, body) {
   body = JSON.parse(body);
