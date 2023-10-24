a295c8886625a18adf129726aa8fc4439c46bb34
nadisan <nadiawellela@gmail.com>
2023-10-24 04:46:49 -0700

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-request(process.argv[2], function(error, response){
-  console.log(response.statusCode);
- }
- )
+
+request(process.argv[2], function (_err, res) {
+  console.log('code:', res.statusCode); // Print the response status code if a response was received
+});
