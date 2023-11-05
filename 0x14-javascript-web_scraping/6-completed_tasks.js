b7211dbcc92c3aa94936d8278e7f70830dbb10df
nadisan <nadiawellela@gmail.com>
2023-11-05 09:03:09 -0800

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,16 +4,16 @@ const request = require('request');
 
 request(process.argv[2], function (error, _response, body) {
   if (error) {
-    console.log(error)
+    console.log(error);
   } else {
     const completedTasksByUsers = {};
     body = JSON.parse(body);
 
-    for (let i = 0; i < body.length; ++i){
+    for (let i = 0; i < body.length; ++i) {
       const userId = body[i].userId;
       const completed = body[i].completed;
 
-      if (completed && !completedTasksByUsers[userId]){
+      if (completed && !completedTasksByUsers[userId]) {
         completedTasksByUsers[userId] = 0;
       }
 
